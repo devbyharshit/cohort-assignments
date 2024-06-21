53c0d58519a7cd271715533bc1792e87cf3d6a45
Harshit Anand <harshitanand283@gmail.com>
2024-06-21 17:24:56 +0530

    todo list solved

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -1,17 +1,41 @@
 /*
-  Implement a class `Todo` having below methods
-    - add(todo): adds todo to list of todos
-    - remove(indexOfTodo): remove todo from list of todos
-    - update(index, updatedTodo): update todo at given index
-    - getAll: returns all todos
-    - get(indexOfTodo): returns todo at given index
-    - clear: deletes all todos
+    Implement a class `Todo` having below methods
+    	- add(todo): adds todo to list of todos
+    	- remove(indexOfTodo): remove todo from list of todos
+    	- update(index, updatedTodo): update todo at given index
+    	- getAll: returns all todos
+    	- get(indexOfTodo): returns todo at given index
+    	- clear: deletes all todos
 
-  Once you've implemented the logic, test your code by running
+    Once you've implemented the logic, test your code by running
 */
 
 class Todo {
+	todos = [];
 
+	add(todo) {
+		this.todos.push(todo);
+	}
+
+	remove(indexOfTodo) {
+		if (indexOfTodo < this.todos.length) this.todos.splice(indexOfTodo, 1);
+	}
+
+	update(index, updatedTodo) {
+		if (index < this.todos.length) this.todos[index] = updatedTodo;
+	}
+
+	getAll() {
+		return this.todos;
+	}
+
+	get(indexOfTodo) {
+		return this.todos[indexOfTodo] || null;
+	}
+
+	clear() {
+		this.todos = [];
+	}
 }
 
 module.exports = Todo;
