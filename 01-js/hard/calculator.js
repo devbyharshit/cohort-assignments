0de403097349961e45d91b064a9639f4152cb479
Harshit Anand <harshitanand283@gmail.com>
2024-06-22 00:02:41 +0530

    calculator solved

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -1,21 +1,56 @@
 /*
-  Implement a class `Calculator` having below methods
-    - initialise a result variable in the constructor and keep updating it after every arithmetic operation
-    - add: takes a number and adds it to the result
-    - subtract: takes a number and subtracts it from the result
-    - multiply: takes a number and multiply it to the result
-    - divide: takes a number and divide it to the result
-    - clear: makes the `result` variable to 0
-    - getResult: returns the value of `result` variable
-    - calculate: takes a string expression which can take multi-arithmetic operations and give its result
-      example input: `10 +   2 *    (   6 - (4 + 1) / 2) + 7`
-      Points to Note: 
+    Implement a class `Calculator` having below methods
+    	- initialise a result variable in the constructor and keep updating it after every arithmetic operation
+    	- add: takes a number and adds it to the result
+    	- subtract: takes a number and subtracts it from the result
+    	- multiply: takes a number and multiply it to the result
+    	- divide: takes a number and divide it to the result
+    	- clear: makes the `result` variable to 0
+    	- getResult: returns the value of `result` variable
+    	- calculate: takes a string expression which can take multi-arithmetic operations and give its result
+    example input: `10 +   2 *    (   6 - (4 + 1) / 2) + 7`
+        Points to Note: 
         1. the input can have multiple continuous spaces, you're supposed to avoid them and parse the expression correctly
         2. the input can have invalid non-numerical characters like `5 + abc`, you're supposed to throw error for such inputs
 
-  Once you've implemented the logic, test your code by running
+    Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+	result = 0;
+
+	add(num) {
+		this.result += num;
+	}
+
+	subtract(num) {
+		this.result -= num;
+	}
+
+	multiply(num) {
+		this.result *= num;
+	}
+
+	divide(num) {
+		if (num === 0) throw new Error("Zero Division Error");
+		this.result /= num;
+	}
+
+	clear() {
+		this.result = 0;
+	}
+
+	getResult() {
+		return this.result;
+	}
+
+	calculate(expression) {
+		const trimmedExp = expression.replace(/[ ]/g, "");
+		const evaluateExp = eval(trimmedExp);
+
+		if (!isFinite(evaluateExp)) throw new Error();
+		else this.result = eval(trimmedExp);
+	}
+}
 
 module.exports = Calculator;
